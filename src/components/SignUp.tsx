204398d327d6b1f200df5660943d43d508e7c0c2
unknown <you@example.com>
2022-09-27 18:03:57 +0400

    secnd commit

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -5,6 +5,10 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 
 interface IFormInput {
     firstName: string;
+    lastName: string;
+    age: number;
+    email: string;
+    password: string;
 }
 
 
@@ -14,12 +18,12 @@ const SignUp: React.FC = () => {
         register,
         formState: {
             errors,
-
+            isValid
         },
         handleSubmit,
         reset
     } = useForm<IFormInput>({
-        mode: 'onBlur',
+        mode: 'all',
     });
 
 
@@ -28,6 +32,7 @@ const SignUp: React.FC = () => {
 
 
     const onSubmit: SubmitHandler<IFormInput> = (data) => {
+        console.log(data)
         alert(JSON.stringify(data))
         reset();
     }
@@ -44,8 +49,48 @@ const SignUp: React.FC = () => {
                         }
                     })} />
                 </label>
+                <label>
+                    lastName:
+                    <input  {...register('lastName', {
+                        required: "What's your name ?",
+                        minLength: {
+                            value: 3,
+                            message: "your username must be between 6 and characters long"
+                        }
+                    })} />
+                </label>
+                <label>
+                    age:
+                    <input  {...register('age', {
+                        required: "What's your name ?",
+                        minLength: {
+                            value: 3,
+                            message: "your username must be between 6 and characters long"
+                        }
+                    })} />
+                </label>
+                <label>
+                    email:
+                    <input  {...register('email', {
+                        required: "What's your name ?",
+                        minLength: {
+                            value: 3,
+                            message: "your username must be between 6 and characters long"
+                        }
+                    })} />
+                </label>
+                <label>
+                    password:
+                    <input  {...register('password', {
+                        required: "What's your name ?",
+                        minLength: {
+                            value: 3,
+                            message: "your username must be between 6 and characters long"
+                        }
+                    })} />
+                </label>
                 <span>{errors?.firstName && (errors?.firstName?.message || 'Error')}</span>
-                <input type="submit" />
+                <input type="submit" disabled={!isValid} />
             </form>
         </div>
     )
